ff57084ab15105e16afa5097905d79f40ab0baa3
Diego <dianmonbla@gmail.com>
2021-01-28 10:45:44 -0500

    Integración y configuración de plantillas HTML para visualziación de mensajes de error y página de inicio. Integración y configuración de ficheros estáticos favicon.ico y estilos. Integración y configuración de rutas inicio y error.

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var createError = require('http-errors'),
   env = require('node-env-file'),
   bodyParser = require('body-parser'), // .env file
   app = express(),
-  cors = require('cors');
+  cors = require('cors'),
+  path = require('path'),
+  favicon = require('serve-favicon');
 
 // Development environment
 if (!process.env.NODE_ENV)
-  env(__dirname + '/.env');  
+  env(__dirname + '/.env');
 
 app.use(cors())
 
@@ -19,9 +21,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Parse application/json
 app.use(bodyParser.json())
 
+// Object keyed by the cookie names
 app.use(cookieParser());
 
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+// Template engine configuration
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 // Routes
+app.use('/', require('./routes/index'));
 app.use('/crm', require('./routes/crm/records'));
 
 // Catch 404 and forward to error handler
@@ -33,7 +46,7 @@ app.use(function (request, response, next) {
 app.use(function (error, request, response, next) {
   // Render the error page
   response.status(error.status || 500);
-  response.json(error);
+  response.render('error', { error, message: error.message });
 });
 
 module.exports = app;
